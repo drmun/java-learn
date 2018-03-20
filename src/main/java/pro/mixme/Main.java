34bb2a2294ed0e38307d43b76b964ac3577c9cf6
F1into <vadim.palits@gmail.com>
2018-03-20 20:40:03 +0300

    Code | Static & No-static Initialisation Blocks, Constructor

---
+++ src/main/java/pro/mixme/Main.java
@@ -0,0 +1,15 @@
+package pro.mixme;
+
+import pro.mixme.Metric.StartTimer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // StartTimer
+        StartTimer startTimer = new StartTimer();
+
+
+    }
+
+}
---
+++ src/main/java/pro/mixme/Metric/StartTimer.java
@@ -0,0 +1,28 @@
+package pro.mixme.Metric;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StartTimer {
+
+    static Date timeStart;
+    static SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm.ss");
+
+    /*
+     * Статический блок инициализации
+     * Отработает первым при инициализации класса
+     */
+    static {
+        timeStart = new Date();
+        System.out.println("1 - Статический блок: \t time > " + dateFormat.format(timeStart));
+    }
+
+    public StartTimer() {
+        System.out.println("3 - Конструктор: \t time > " + dateFormat.format(timeStart));
+    }
+
+    {
+        System.out.println("2 - Блок инициализации  \t time > " + dateFormat.format(timeStart));
+    }
+
+}
