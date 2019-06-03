f34c1a85ade9d2f7c31999894e0913391cc96a72
F1into <ingener58>
2019-06-04 00:38:12 +0300

    [master] javadoc

--- src/main/test/pro/mixme/MessageControllerTest.java
+++ src/main/test/pro/mixme/MessageControllerTest.java
@@ -6,13 +6,28 @@ import pro.mixme.saver.ConsoleSaver;
 
 import static org.junit.Assert.*;
 
+/**
+ * @Before
+ * Метод, помеченный аннотацией @Before, отрабатывает перед каждым тестом
+ * В нем описывается общее для всех тестов поведение
+ *
+ * @After
+ * Метод, помеченный аннотацией @After, отрабатывает после каждого теста
+ * Как правило используется для обнуления состояний и очистки даннных
+ *
+ * @Test
+ * Аннотацией @Test помечается метод теста.
+ * Тестовый метод должен быть public void.
+ * Существуют параметризованные тесты, это отдельная тема.
+ * Методы класса org.junit.Assert проверяют результат выполнения.
+ *
+ * @Test(expected = IOException.class)
+ * Если ожидается выброс исключения, к аннотации добавляется параметр
+ */
 public class MessageControllerTest {
 
     private MessageController messageController;
 
-    /**
-     * Метод, помеченный аннотацией @Before, отрабатывает перед каждым тестом
-     */
     @Before
     public void setUp() throws Exception {
         messageController = new MessageController();
